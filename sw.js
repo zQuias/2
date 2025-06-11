e72bb0284a8f3d376ab2456263d69a02f01a7635
Ezequias Nascimento <zQuiasSilva@gmail.com>
2025-06-11 09:04:06 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
+'https://zquias.github.io/2/',
+'https://zquias.github.io/2/index.html',
   // CDN resources que são essenciais
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
